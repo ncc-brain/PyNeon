0c538fa262f304bcf76027db190fa498bdb66fa2
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-01-24 16:37:41 +0100

    add sample data CircleFlash

--- pyneon/utils/sample_data.py
+++ pyneon/utils/sample_data.py
@@ -7,6 +7,7 @@ data_dir = Path(__file__).parent.parent.parent / "data"
 data_url_dict = {
     "boardView": "https://osf.io/download/t52b4/",
     "ArtLab": "https://osf.io/download/nb53p/",
+    "CircleFlash": "https://osf.io/download/6793b3540717d27236b52d7f/"
 }
 
 
