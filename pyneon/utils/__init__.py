a4ff9d620ea4a04490817c127d9188e1920a6e82
Qian Chu <97355086+qian-chu@users.noreply.github.com>
2025-10-28 20:03:50 +0100

    start formalize load_native_stream

--- pyneon/utils/__init__.py
+++ pyneon/utils/__init__.py
@@ -1,4 +1,4 @@
-from .utils import load_native_data, _check_data, load_or_compute
+from .utils import load_native_stream, _check_data, load_or_compute
 from .sample_data import get_sample_data
 from .variables import *
 
